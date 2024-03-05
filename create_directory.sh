346a8d6f2c9a59fda6c84623d5b7aac998589455
Hyunjin Hwang <159972266+codepresso-gcrnd@users.noreply.github.com>
2024-03-05 22:06:38 +0900

    Create create_directory.sh for Level directories

---
+++ create_directory.sh
@@ -0,0 +1,24 @@
+# create beginner directory
+# set_02
+mkdir -p beginner/set_01/01 && touch beginner/set_01/01/main.md
+mkdir -p beginner/set_01/02 && touch beginner/set_01/02/main.md
+mkdir -p beginner/set_01/03 && touch beginner/set_01/03/main.md
+mkdir -p beginner/set_01/04 && touch beginner/set_01/04/main.md
+mkdir -p beginner/set_01/05 && touch beginner/set_01/05/main.md
+
+# create intermediate directory
+# set_01
+mkdir -p intermediate/set_01/01 && touch intermediate/set_01/01/main.md
+mkdir -p intermediate/set_01/02 && touch intermediate/set_01/02/main.md
+mkdir -p intermediate/set_01/03 && touch intermediate/set_01/03/main.md
+mkdir -p intermediate/set_01/04 && touch intermediate/set_01/04/main.md
+mkdir -p intermediate/set_01/05 && touch intermediate/set_01/05/main.md
+
+# create professional directory
+# set_01
+mkdir -p professional/set_01/01 && touch professional/set_01/01/main.md
+mkdir -p professional/set_01/02 && touch professional/set_01/02/main.md
+mkdir -p professional/set_01/03 && touch professional/set_01/03/main.md
+mkdir -p professional/set_01/04 && touch professional/set_01/04/main.md
+mkdir -p professional/set_01/05 && touch professional/set_01/05/main.md
+ 
