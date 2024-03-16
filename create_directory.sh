cba0b2c72f964a618a52bd65c3c4a463b8fc127b
Hyunjin Hwang <159972266+codepresso-gcrnd@users.noreply.github.com>
2024-03-16 19:15:46 +0900

    Update create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -32,12 +32,24 @@ mkdir -p intermediate/set_01/02 && touch intermediate/set_01/02/main.md
 mkdir -p intermediate/set_01/03 && touch intermediate/set_01/03/main.md
 mkdir -p intermediate/set_01/04 && touch intermediate/set_01/04/main.md
 mkdir -p intermediate/set_01/05 && touch intermediate/set_01/05/main.md
+mkdir -p intermediate/set_01/06 && touch intermediate/set_01/06/main.md
+mkdir -p intermediate/set_01/07 && touch intermediate/set_01/07/main.md
+mkdir -p intermediate/set_01/08 && touch intermediate/set_01/08/main.md
+mkdir -p intermediate/set_01/09 && touch intermediate/set_01/09/main.md
+mkdir -p intermediate/set_01/10 && touch intermediate/set_01/10/main.md
+
 # set_02
 mkdir -p intermediate/set_02/01 && touch intermediate/set_02/01/main.md
 mkdir -p intermediate/set_02/02 && touch intermediate/set_02/02/main.md
 mkdir -p intermediate/set_02/03 && touch intermediate/set_02/03/main.md
 mkdir -p intermediate/set_02/04 && touch intermediate/set_02/04/main.md
 mkdir -p intermediate/set_02/05 && touch intermediate/set_02/05/main.md
+
+mkdir -p intermediate/set_02/06 && touch intermediate/set_02/06/main.md
+mkdir -p intermediate/set_02/07 && touch intermediate/set_02/07/main.md
+mkdir -p intermediate/set_02/08 && touch intermediate/set_02/08/main.md
+mkdir -p intermediate/set_02/09 && touch intermediate/set_02/09/main.md
+mkdir -p intermediate/set_02/10 && touch intermediate/set_02/10/main.md
 
 # create professional directory
 # set_01
