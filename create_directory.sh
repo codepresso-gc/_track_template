58f7a85c06d9095d256c3642d84ff8232b0ac9e5
Hyunjin Hwang <159972266+codepresso-gcrnd@users.noreply.github.com>
2024-03-29 15:12:36 +0900

    Update create_directory.sh

--- create_directory.sh
+++ create_directory.sh
@@ -25,6 +25,18 @@ mkdir -p beginner/set_02/08 && touch beginner/set_02/08/main.md
 mkdir -p beginner/set_02/09 && touch beginner/set_02/09/main.md
 mkdir -p beginner/set_02/10 && touch beginner/set_02/10/main.md
 
+# set_03
+mkdir -p beginner/set_03/01 && touch beginner/set_03/01/main.md
+mkdir -p beginner/set_03/02 && touch beginner/set_03/02/main.md
+mkdir -p beginner/set_03/03 && touch beginner/set_03/03/main.md
+mkdir -p beginner/set_03/04 && touch beginner/set_03/04/main.md
+mkdir -p beginner/set_03/05 && touch beginner/set_03/05/main.md
+mkdir -p beginner/set_03/06 && touch beginner/set_03/06/main.md
+mkdir -p beginner/set_03/07 && touch beginner/set_03/07/main.md
+mkdir -p beginner/set_03/08 && touch beginner/set_03/08/main.md
+mkdir -p beginner/set_03/09 && touch beginner/set_03/09/main.md
+mkdir -p beginner/set_03/10 && touch beginner/set_03/10/main.md
+
 # create intermediate directory
 # set_01
 mkdir -p intermediate/set_01/01 && touch intermediate/set_01/01/main.md
@@ -44,12 +56,23 @@ mkdir -p intermediate/set_02/02 && touch intermediate/set_02/02/main.md
 mkdir -p intermediate/set_02/03 && touch intermediate/set_02/03/main.md
 mkdir -p intermediate/set_02/04 && touch intermediate/set_02/04/main.md
 mkdir -p intermediate/set_02/05 && touch intermediate/set_02/05/main.md
-
 mkdir -p intermediate/set_02/06 && touch intermediate/set_02/06/main.md
 mkdir -p intermediate/set_02/07 && touch intermediate/set_02/07/main.md
 mkdir -p intermediate/set_02/08 && touch intermediate/set_02/08/main.md
 mkdir -p intermediate/set_02/09 && touch intermediate/set_02/09/main.md
 mkdir -p intermediate/set_02/10 && touch intermediate/set_02/10/main.md
+
+# set_03
+mkdir -p intermediate/set_03/01 && touch intermediate/set_03/01/main.md
+mkdir -p intermediate/set_03/02 && touch intermediate/set_03/02/main.md
+mkdir -p intermediate/set_03/03 && touch intermediate/set_03/03/main.md
+mkdir -p intermediate/set_03/04 && touch intermediate/set_03/04/main.md
+mkdir -p intermediate/set_03/05 && touch intermediate/set_03/05/main.md
+mkdir -p intermediate/set_03/06 && touch intermediate/set_03/06/main.md
+mkdir -p intermediate/set_03/07 && touch intermediate/set_03/07/main.md
+mkdir -p intermediate/set_03/08 && touch intermediate/set_03/08/main.md
+mkdir -p intermediate/set_03/09 && touch intermediate/set_03/09/main.md
+mkdir -p intermediate/set_03/10 && touch intermediate/set_03/10/main.md
 
 # create professional directory
 # set_01
